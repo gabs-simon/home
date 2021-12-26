06e78b4147f5c817f284fbe6842a9172f56bd8c1
Gabs <gabs@gabs.work>
2021-12-25 23:32:46 -0800

    Yarn removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Window from '../components/terminal/Window'
+import Window from '../components/terminal/TerminalWindow'
 
 export default function Home() {
   return (<>
